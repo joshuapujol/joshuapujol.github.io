2c9c5583ce11c6370bccc2367e182cd2160c771b
Joshua Pujol <jpujol@deakin.edu.au>
2018-12-23 18:27:47 +1100

    Various JS functions
    Added randomly generated backgrounds
    
    Various bug fixes
    
    Added more social buttons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,78 @@
-window.stars = function () {
-    var stars = $("<div id='stars'></div><div id='stars2'></div><div id='stars3'></div>");
+random = 0;
 
-    return stars;
 
-};
+$(document).ready(function(){
+    
+    
+    var exists = [],
+    randomInt;
+    // Randomly generated number.
+    $(".palette").click(function(){      
+        
+//        random = Math.floor((Math.random() * 3) + 1);
+//        console.log(random)
+    // Random Non-Repeating Number Generator
+    function randomBGInt() {
+
+
+        for (var l = 0; l < 6; l++) {
+            do {
+
+                randomInt = Math.floor(Math.random() * 6 + 1);
+            } while (exists[randomInt]);
+            exists[randomInt] = true; {
+                       
+                return randomInt;
+                
+            }
+        }
+    }
+
+        
+    randomBGInt(); 
+        
+         console.log(randomInt);
+        switch(randomInt) {
+            case 1:
+                $("main").css({
+                "backgroundColor": "#FF80AB",
+                "color": "white"});          
+            break;
+                
+            case 2: 
+                $("main").css({
+                "backgroundColor": "#7b68ee",
+                "color": "white"});
+            break;
+            
+            case 3: 
+                $("main").css({
+                "backgroundColor": "#00B8D4",
+                "color": "white"}); 
+            break;
+            
+            case 4:
+                $("main").css({
+                "backgroundColor": "black",
+                "color": "white"});          
+            break;
+                
+            case 5: 
+                $("main").css({
+                "backgroundColor": "#64DD17",
+                "color": "black"});
+            break;
+            
+            case 6: 
+                $("main").css({
+                "backgroundColor": "#FFEA00",
+                "color": "white"}); 
+            break;
+        }
+        
+        
+    });
+    
+    
+    
+});
